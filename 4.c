d873310166ee383446fe487b0a07015e5301c90b
Jayakumar-Vaithiyashankar <45353283+jayakumarksrit@users.noreply.github.com>
2025-08-14 16:39:09 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() 
+{
+    int my_array[5]={8,3,4,6,2};//array declaration
+    printf("\n  element[%d]=%d",0,my_array[0]);
+    printf("\n  element[%d]=%d",1,my_array[1]);
+    printf("\n  element[%d]=%d",2,my_array[2]);
+    printf("\n  element[%d]=%d",3,my_array[3]);
+    printf("\n  element[%d]=%d",4,my_array[4]);
+}
